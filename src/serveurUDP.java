70c74c1ccf08ad10e0550083b2682452f5267d60
mponton <mponton@grandlyon.com>
2024-03-21 10:39:15 +0100

    users in new thread

--- src/serveurUDP.java
+++ src/serveurUDP.java
@@ -24,15 +24,25 @@ public class serveurUDP {
 				String str = new String(packet.getData()).trim();
 				// Si le message reçu est "hello serveur RX302"
 				if (str.equals("hello serveur RX302")) {
-					// Affichage de l'adresse et du port du client
 					System.out.println("Nouveau client : " + packet.getAddress() + ":" + packet.getPort());
+					//We open a new thread to handle the client
+					new Thread(() -> {
+						try {
+							int id = (int) (Math.random() * 1000);
+							// Création d'un message de réponse
 
-					// Création d'un message de réponse
-					byte[] buffer2 = "Serveur RX302 ready".getBytes();
-					// Création d'un paquet pour envoyer la réponse
-					DatagramPacket packet2 = new DatagramPacket(buffer2, buffer2.length, packet.getAddress(), packet.getPort());
-					// Envoi de la réponse
-					server.send(packet2);
+							//we concatenate the message with the id
+							String Welcome_message = "You have been assigned the ID: " + id + ". Serveur RX302 ready";
+							byte[] buffer2 = Welcome_message.getBytes();
+							// Création d'un paquet pour envoyer la réponse
+							DatagramPacket packet2 = new DatagramPacket(buffer2, buffer2.length, packet.getAddress(), packet.getPort());
+							// Envoi de la réponse
+							server.send(packet2);
+						} catch (IOException e) {
+							// Gestion des exceptions liées aux entrées/sorties
+							e.printStackTrace();
+						}
+					}).start();
 				} else {
 					// Si le message reçu n'est pas "hello serveur RX302", affichage du message et renvoi du même message
 					System.out.println("Message from client: " + str);
